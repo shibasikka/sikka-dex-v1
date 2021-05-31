fa999a4e1ea3e65f60e89ab551931c5df7d49cb3
WaaS Hero <51007483+IDB-Media@users.noreply.github.com>
2021-05-31 13:20:42 -0700

    update to sikka

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,114 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://slipstream.exchange/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://slipstream.exchange/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: 'https://slipstream.exchange/syrup',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://alttraderpro.exchange/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: 'https://alttraderpro.exchange/nft',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   calloutClass: 'rainbow',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: 'https://alttraderpro.exchange/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: 'https://alttraderpro.exchange/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: 'https://alttraderpro.exchange/profile',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'Overview',
+  //       href: 'https://pancakeswap.info',
+  //     },
+  //     {
+  //       label: 'Tokens',
+  //       href: 'https://pancakeswap.info/tokens',
+  //     },
+  //     {
+  //       label: 'Pairs',
+  //       href: 'https://pancakeswap.info/pairs',
+  //     },
+  //     {
+  //       label: 'Accounts',
+  //       href: 'https://pancakeswap.info/accounts',
+  //     },
+  //   ],
+  // },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: 'https://alttraderpro.exchange/ifo',
+  // },
+  // {
+  //   label: 'More',
+  //   icon: 'MoreIcon',
+  //   items: [
+  //     {
+  //       label: 'Voting',
+  //       href: 'https://voting.alttraderpro.exchange',
+  //     },
+  //     {
+  //       label: 'Github',
+  //       href: 'https://github.com/pancakeswap',
+  //     },
+  //     {
+  //       label: 'Docs',
+  //       href: 'https://docs.alttraderpro.exchange',
+  //     },
+  //     {
+  //       label: 'Blog',
+  //       href: 'https://pancakeswap.medium.com',
+  //     },
+  //   ],
+  // },
+]
+
+export default config
